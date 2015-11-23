26362b41f9f81c536ad80a17e1148a5730b351b3
angelo marano <amarano@gmail.com>
2015-11-23 07:40:17 -0500

    init commit

---
+++ src/Tpam/View.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tpam
+{
+    public abstract class View<T>
+    {
+        private readonly T _t;
+
+        public View(T t)
+        {
+            _t = t;
+            Serializer = o => Task.Factory.StartNew<string>(() => JsonConvert.SerializeObject(o));
+
+        }
+
+        public View(T t, Func<T, Task<string>> serializer) : this(t)
+        {
+            Serializer = serializer;
+        }
+
+        protected virtual Func<T, Task<string>> Serializer { get; set; }
+
+        protected async virtual Task<string> Present()
+        {
+            return await Serializer(_t);
+        }
+    }
+}
