fae7d00ea5bff245768ce6388ee5caf29bc3f30c
angelo marano <amarano@gmail.com>
2015-11-23 19:49:46 -0500

    added repository classes

---
+++ src/Tpam.EntityFramework/EntityFrameworkRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Tpam.EntityFramework
+{
+    public class EntityFrameworkRepository<Tkey, T> : IRepository<T>, IReadRepository<T>
+    {
+        public EntityFrameworkRepository()
+        {
+
+        }
+
+        bool IRepository<T>.Delete(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> IReadRepository<T>.Fetch()
+        {
+            throw new NotImplementedException();
+        }
+
+        T IReadRepository<T>.Fetch<TKey>(TKey key, Expression<Func<T, TKey>> keySelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        TKey IRepository<T>.Save<TKey>(T t, Expression<Func<T, TKey>> keySelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IRepository<T>.Update<TKey>(T t, Expression<Func<T, TKey>> keySelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> IReadRepository<T>.Where(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Tpam/IRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Tpam
+{
+    public interface IRepository<T> : IReadRepository<T>
+    {
+        TKey Save<TKey>(T t, Expression<Func<T, TKey>> keySelector);
+        bool Update<TKey>(T t, Expression<Func<T, TKey>> keySelector);
+        bool Delete(T t);
+
+    }
+
+    public interface IReadRepository<T>
+    {
+        IEnumerable<T> Fetch();
+        T Fetch<TKey>(TKey key, Expression<Func<T, TKey>> keySelector);
+        IEnumerable<T> Where(Expression<Func<T, bool>> predicate); 
+    }
+
+    public class ReadRepositoryFacade<T, TKey> 
+    {
+        private readonly IReadRepository<T> _repository;
+        private readonly Expression<Func<T, TKey>> _keySelector;
+
+        public ReadRepositoryFacade(IReadRepository<T> repository, Expression<Func<T, TKey>> keySelector)
+        {
+            _repository = repository;
+            _keySelector = keySelector;
+        }
+
+        public virtual IEnumerable<T> Fetch()
+        {
+            return _repository.Fetch();
+        }
+
+        public virtual T Fetch(TKey id)
+        {
+            return _repository.Fetch(id, _keySelector);
+        }
+
+    }
+
+}
