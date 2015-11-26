3b01cc8a0b72805de1eb251f6395ba036c360618
angelo marano <amarano@gmail.com>
2015-11-26 09:23:08 -0500

    concurrent collections

---
+++ src/Tpam/Data/InMemoryRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+
+namespace Tpam.Data
+{
+    public class InMemoryRepository<T> : IRepository<T>, IReadRepository<T>
+    {
+        ConcurrentBag<T> _bag = new ConcurrentBag<T>();
+
+        public InMemoryRepository(IEnumerable<T> objects)
+        {
+            _bag = new ConcurrentBag<T>(objects);
+        }
+
+        bool IRepository<T>.Delete(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        ReadRepositoryFacade<T, TKey> IReadRepository<T>.Facade<TKey>(Expression<Func<T, TKey>> keySelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        RepositoryFacade<T, TKey> IRepository<T>.Facade<TKey>(Expression<Func<T, TKey>> keySelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> IReadRepository<T>.Fetch()
+        {
+            throw new NotImplementedException();
+        }
+
+        T IReadRepository<T>.Fetch<TKey>(TKey key, Expression<Func<T, TKey>> keySelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        TKey IRepository<T>.Save<TKey>(T t, Expression<Func<T, TKey>> keySelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IRepository<T>.Update<TKey>(T t, Expression<Func<T, TKey>> keySelector)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> IReadRepository<T>.Where(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
